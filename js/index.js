733e09a4e1786ddcded5e825b1467a7e436ec8f4
Tylerjkav <69436227+Tylerjkav@users.noreply.github.com>
2021-01-24 14:46:23 -0700

    made adjustments
    fixed some design issues and added another js element

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ $(".toggle").on("click", function(){
     $("#toggleDown").slideToggle("slow");
   });
   
+  $("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });  
