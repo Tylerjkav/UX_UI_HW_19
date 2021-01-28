2a1f195a7291a02c71bf0e3d7d6a6bc9bedde179
Tylerjkav <69436227+Tylerjkav@users.noreply.github.com>
2021-01-27 20:36:36 -0700

    Homework 19 finish
    Completed homework assignment

--- js/index.js
+++ js/index.js
@@ -5,3 +5,29 @@ $(".toggle").on("click", function(){
   $("a[href='#top']").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   });  
+
+  // $("a[href='#work']").click(function() {
+  //   $("html, body").animate({ scrollTop: 50 }, "slow");
+  // });  
+
+
+  // $("#work").click(function (){
+  //    $('html, body').animate({
+  //       scrollTarget: $("#workStart").offset().top
+  //     },  1000);
+  // });
+
+//   $("#work").click(function (){
+//     $('html, body').animate({
+//        scrollTop: $("#workStart").offset().top
+//      },  1000);
+//  });
+
+
+
+
+$("#work").click(function (){
+  $('html, body').animate({
+    scrollTop: ($('#workStart').offset().top)
+  },500);
+});
